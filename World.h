13ab5f7510d7a6562bfd421138723ca8b4769c08
inet2005 <joshuabowersco@gmail.com>
2017-12-05 15:17:45 -0400

    Fixed memory issue

--- World.h
+++ World.h
@@ -11,7 +11,7 @@ class Organism;
 
 const int MAXROW = 20;
 const int MAXCOL = 20;
-const int INITHUMANS = 1;//Initial population of humans
+const int INITHUMANS = 100;//Initial population of humans
 const int INITZOMBIES = 5;//Initial population of zombies
 const int SPAWNHUMANS = 3;//A step in time when humans spawn
 const int SPAWNZOMBIES = 8;//A step in time when zombies spawn
